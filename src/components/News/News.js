80f3c8f514ae5eeb55b78a26817454d9079fc9c6
Cesar David Betancourt Adame <cesarebetan@gmail.com>
2018-04-21 18:29:33 -0500

    Add combo box to select stock and change stock names in marquee. Add key to props

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -21,15 +21,14 @@ class News extends Component {
 
     render() {
         let date = new Date();
-        console.log(date);
         let options = {month: 'long'};
         let fecha = date.getUTCDate() + " de " + date.toLocaleDateString('es-Mx', options) + " de " + date.getFullYear();
         return (
             <div id="news-container" className="col-sm-3">
-                <div class="dashhead m-t-md">
-                    <div class="dashhead-titles">
-                        <h6 class="dashhead-subtitle">{fecha}</h6>
-                        <h2 class="dashhead-title">Noticias</h2>
+                <div className="dashhead m-t-md">
+                    <div className="dashhead-titles">
+                        <h6 className="dashhead-subtitle">{fecha}</h6>
+                        <h2 className="dashhead-title">Noticias</h2>
                     </div>
                 </div>
                 <NewsList news={this.state.news}/>
